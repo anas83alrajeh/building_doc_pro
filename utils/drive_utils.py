6f546666ef2edeac8c2abd099cceeca9e1debf3e
Anas Al Rajeh <160426061+anas83alrajeh@users.noreply.github.com>
2025-06-15 22:57:49 +0200

    Update drive_utils.py

--- utils/drive_utils.py
+++ utils/drive_utils.py
@@ -1,29 +1,17 @@
-# utils/drive_utils.py
-
-import os
-import json
+import os, json, io
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 def get_gdrive_service():
-    service_account_info = json.loads(os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"])
-    credentials = service_account.Credentials.from_service_account_info(
-        service_account_info,
-        scopes=["https://www.googleapis.com/auth/drive"]
+    svc = json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON"))
+    creds = service_account.Credentials.from_service_account_info(
+        svc, scopes=["https://www.googleapis.com/auth/drive"]
     )
-    return build('drive', 'v3', credentials=credentials)
+    return build("drive", "v3", credentials=creds)
 
 def upload_file_to_drive(file_path, file_name, folder_id):
-    service = get_gdrive_service()
-    file_metadata = {
-        'name': file_name,
-        'parents': [folder_id]
-    }
+    svc = get_gdrive_service()
+    metadata = {"name": file_name, "parents": [folder_id]}
     media = MediaFileUpload(file_path, resumable=True)
-    uploaded_file = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id'
-    ).execute()
-    return uploaded_file.get('id')
+    return svc.files().create(body=metadata, media_body=media, fields="id").execute().get("id")
